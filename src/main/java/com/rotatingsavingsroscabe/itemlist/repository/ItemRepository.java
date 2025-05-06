350065646e4f27d4d13ae9227e18a7eb162ac29e
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-05-06 23:59:07 +0530

    feat: Update ItemRepository to extend JpaRepository and add newline in ItemlistApplication

--- src/main/java/com/rotatingsavingsroscabe/itemlist/repository/ItemRepository.java
+++ src/main/java/com/rotatingsavingsroscabe/itemlist/repository/ItemRepository.java
@@ -1,4 +1,8 @@
 package com.rotatingsavingsroscabe.itemlist.repository;
 
-public interface ItemRepository {
+import com.rotatingsavingsroscabe.itemlist.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
 }
