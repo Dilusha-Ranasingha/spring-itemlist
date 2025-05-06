e0f9d95948f72cbab8b97b9b1b307100358e3e67
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-05-06 23:59:17 +0530

    feat: Implement ItemController with endpoints to get and add items

--- src/main/java/com/rotatingsavingsroscabe/itemlist/controller/ItemController.java
+++ src/main/java/com/rotatingsavingsroscabe/itemlist/controller/ItemController.java
@@ -1,4 +1,29 @@
 package com.rotatingsavingsroscabe.itemlist.controller;
 
+import com.rotatingsavingsroscabe.itemlist.model.Item;
+import com.rotatingsavingsroscabe.itemlist.repository.ItemRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5173") // Allow React app to call this API
+@RestController
+@RequestMapping("/items")
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    @PostMapping
+    public Item addItem(@RequestBody Item item) {
+        return itemRepository.save(item);
+    }
 }
