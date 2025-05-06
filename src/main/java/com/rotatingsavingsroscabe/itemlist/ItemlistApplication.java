83bda91c6d3fe67d4fe2f7751ed9f22105e28a17
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-05-06 23:47:15 +0530

    initial commit

---
+++ src/main/java/com/rotatingsavingsroscabe/itemlist/ItemlistApplication.java
@@ -0,0 +1,13 @@
+package com.rotatingsavingsroscabe.itemlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemlistApplication.class, args);
+    }
+
+}
